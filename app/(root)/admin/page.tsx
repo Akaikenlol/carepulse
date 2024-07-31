0b2be0fd0d7b999ae277a87cb3f4321113bd2658
Akaikenlol <keithken972@gmail.com>
2024-07-31 21:24:47 +0630

    feat: Implementing Sentry chore: Bug Fixed

--- app/(root)/admin/page.tsx
+++ app/(root)/admin/page.tsx
@@ -1,22 +1,23 @@
-import StatCard from "@/components/StatCard";
+import Image from "next/image";
+import Link from "next/link";
+
+import { StatCard } from "@/components/StatCard";
 import { columns } from "@/components/table/columns";
 import { DataTable } from "@/components/table/DataTable";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
 
-const Admin = async () => {
+const AdminPage = async () => {
 	const appointments = await getRecentAppointmentList();
+
 	return (
 		<div className="mx-auto flex max-w-7xl flex-col space-y-14">
 			<header className="admin-header">
-				<Link href={"/"} className="cursor-pointer">
+				<Link href="/" className="cursor-pointer">
 					<Image
-						src={"/assets/icons/logo-full.svg"}
+						src="/assets/icons/logo-full.svg"
 						height={32}
 						width={162}
-						alt="Logo"
+						alt="logo"
 						className="h-8 w-fit"
 					/>
 				</Link>
@@ -26,36 +27,37 @@ const Admin = async () => {
 
 			<main className="admin-main">
 				<section className="w-full space-y-4">
-					<h1 className="header">Welcome 👏</h1>
+					<h1 className="header">Welcome 👋</h1>
 					<p className="text-dark-700">
-						Start the day with managing new appointment.
+						Start the day with managing new appointments
 					</p>
 				</section>
 
 				<section className="admin-stat">
 					<StatCard
-						type="appointment"
+						type="appointments"
 						count={appointments.scheduledCount}
-						label="Schedule appointments"
-						icon="/assets/icons/appointments.svg"
+						label="Scheduled appointments"
+						icon={"/assets/icons/appointments.svg"}
 					/>
 					<StatCard
 						type="pending"
 						count={appointments.pendingCount}
 						label="Pending appointments"
-						icon="/assets/icons/pending.svg"
+						icon={"/assets/icons/pending.svg"}
 					/>
 					<StatCard
 						type="cancelled"
 						count={appointments.cancelledCount}
 						label="Cancelled appointments"
-						icon="/assets/icons/cancelled.svg"
+						icon={"/assets/icons/cancelled.svg"}
 					/>
 				</section>
+
 				<DataTable columns={columns} data={appointments.documents} />
 			</main>
 		</div>
 	);
 };
 
-export default Admin;
+export default AdminPage;
